257f8b9bf20943eb4b3771832b9d8b78f5e2fbb6
Saeed S. Lafmejani <113765583+ssldk83@users.noreply.github.com>
2025-08-08 23:34:04 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import EnergyApp from './EnergyApp.jsx'
+import './index.css' // Tailwind or shadcn styles
 
-createRoot(document.getElementById('root')).render(<App />)
+createRoot(document.getElementById('root')).render(
+  <EnergyApp />
+)
