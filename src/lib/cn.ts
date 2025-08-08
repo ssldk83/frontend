95bc8f3a4d4280f0b650e215b20fbb60e39ce045
Saeed S. Lafmejani <113765583+ssldk83@users.noreply.github.com>
2025-08-09 00:03:18 +0200

    Update cn.ts

--- src/lib/cn.ts
+++ src/lib/cn.ts
@@ -1,3 +1,4 @@
 export function cn(...classes: (string|undefined|false|null)[]) {
   return classes.filter(Boolean).join(" ");
 }
+export function cn(...classes){ return classes.filter(Boolean).join(" "); }
