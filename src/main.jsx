bad122338e357bb75f775444099da0a7578bc9ee
Saeed S. Lafmejani <113765583+ssldk83@users.noreply.github.com>
2025-08-08 23:59:00 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
-import EnergyApp from './EnergyApp.jsx'
-import './index.css' // Tailwind or shadcn styles
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
 
-createRoot(document.getElementById('root')).render(
-  <EnergyApp />
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
